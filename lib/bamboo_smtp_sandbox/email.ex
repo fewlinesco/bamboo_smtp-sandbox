59a554d88f021b255eb24b4e8ccef7d29987130c
Arnaud Morisset <arnaudmorisset@protonmail.com>
2020-11-05 09:36:39 +0100

    Add a function to send an email with attachments

--- lib/bamboo_smtp_sandbox/email.ex
+++ lib/bamboo_smtp_sandbox/email.ex
@@ -9,4 +9,19 @@ defmodule BambooSMTPSandbox.Email do
       text_body: text_body
     )
   end
+
+  def sample_email_with_attachments(subject \\ "BambooSMTP Sandbox", text_body \\ "Hello 👋") do
+    sample_email = sample_email(subject, text_body)
+    attachments = Path.wildcard("priv/attachments/*.*")
+
+    add_attachments(sample_email, attachments)
+  end
+
+  defp add_attachments(email, []), do: email
+
+  defp add_attachments(email, [attachment | attachments]) do
+    email
+    |> put_attachment(attachment)
+    |> add_attachments(attachments)
+  end
 end
--- lib/bamboo_smtp_sandbox/sender.ex
+++ lib/bamboo_smtp_sandbox/sender.ex
@@ -4,4 +4,10 @@ defmodule BambooSMTPSandbox.Sender do
 
     BambooSMTPSandbox.Mailer.deliver_now(email)
   end
+
+  def send_sample_email_with_attachments do
+    email = BambooSMTPSandbox.Email.sample_email_with_attachments()
+
+    BambooSMTPSandbox.Mailer.deliver_now(email)
+  end
 end
